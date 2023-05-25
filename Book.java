b6f2254416f2142468c3d515919e6ab0c5314c9b
soofeee <120269099+soofeee@users.noreply.github.com>
2023-05-25 21:50:20 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author aaloo
+ */
+public class Book {
+    
+    private String book;
+    private double price;
+  
+    public Book(String book,double price){
+        this.book=book;
+        this.price=price;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
+    /*public void bookList (){
+        System.out.println(book+" "+id+" "+price+" "+author);
+ 
+    }*/
+    
+    public String toString(){
+        return"Book name: "+book+", Price: "+price;
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.util.*;
+
+/**
+ *
+ * @author aaloo
+ */
+public class Employee extends User {
+    
+     public Employee(String name,int number,String pass){
+        super(name,number,pass);
+    }
+         
+    
+   
+     }
