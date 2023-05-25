4e7de8b8b8cbcf383d0fa8dd7c1c7fe6ac3dac6a
soofeee <120269099+soofeee@users.noreply.github.com>
2023-05-25 18:57:12 +0000

    up1

--- Book.java
+++ Book.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package project;
+package Library;
 
-/**
- *
- * @author aaloo
- */
+
 public class Book {
     
     private String book;
